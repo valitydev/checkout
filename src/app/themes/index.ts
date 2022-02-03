d1fb72b6e89039f5dcf060e23c17711e74a25c42
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-02-03 11:13:27 +0300

    fixes

--- src/app/themes/index.ts
+++ src/app/themes/index.ts
@@ -1,22 +1,21 @@
-import { Theme } from "./theme";
-import { ThemeName } from "./theme-name";
-import main from "./main";
-import coral from "./coral";
-import persianGreen from "./persian-green";
-import solitude from "./solitude";
-import eastern from "./eastern";
+import { Theme } from './theme';
+import { ThemeName } from './theme-name';
+import main from './main';
+import coral from './coral';
+import persianGreen from './persian-green';
+import solitude from './solitude';
+import eastern from './eastern';
 
 const themes = [main, coral, persianGreen, solitude, eastern];
 
 export const DEFAULT_THEME = main;
 
 export interface WithThemeProps {
-  theme: Theme;
+    theme: Theme;
 }
 
 export function getTheme(themeName: ThemeName): Theme {
-  return themes.find(({ name }) => name === themeName) || DEFAULT_THEME;
+    return themes.find(({ name }) => name === themeName) || DEFAULT_THEME;
 }
 
-export { ThemeName };
-export type { Theme };
+export { ThemeName, Theme };
