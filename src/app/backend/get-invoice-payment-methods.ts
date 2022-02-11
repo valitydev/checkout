84792bf570ce77e75da2dae5d56db2a60592f042
Ildar Galeev <KeinAsylum@gmail.com>
2022-02-11 18:04:24 +0300

    APM-5: Removed mock

--- src/app/backend/get-invoice-payment-methods.ts
+++ src/app/backend/get-invoice-payment-methods.ts
@@ -1,31 +1,12 @@
-import { PaymentMethod, fetchCapi, PaymentMethodName } from '.';
+import { PaymentMethod, fetchCapi } from '.';
 import v from './capi-version';
-
-// export const getInvoicePaymentMethods = (
-//     capiEndpoint: string,
-//     accessToken: string,
-//     invoiceID: string
-// ): Promise<PaymentMethod[]> =>
-//     fetchCapi({
-//         endpoint: `${capiEndpoint}/${v}/processing/invoices/${invoiceID}/payment-methods`,
-//         accessToken
-//     });
-
-const case2 = [
-    {
-        method: PaymentMethodName.BankCard
-    },
-    {
-        method: PaymentMethodName.DigitalWallet,
-        providers: ['sticpay']
-    }
-];
 
 export const getInvoicePaymentMethods = (
     capiEndpoint: string,
     accessToken: string,
     invoiceID: string
-): Promise<PaymentMethod[]> => {
-    console.log(capiEndpoint, accessToken, invoiceID, v);
-    return Promise.resolve(case2);
-};
+): Promise<PaymentMethod[]> =>
+    fetchCapi({
+        endpoint: `${capiEndpoint}/${v}/processing/invoices/${invoiceID}/payment-methods`,
+        accessToken
+    });
