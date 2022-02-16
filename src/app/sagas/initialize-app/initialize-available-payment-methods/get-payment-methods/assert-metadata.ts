03be8beeb6ccb750a6abb8439a7224ce52ea5623
Rinat Arsaev <11846445+KrickRay@users.noreply.github.com>
2022-02-16 23:10:08 +0300

    logPrefix

--- src/app/sagas/initialize-app/initialize-available-payment-methods/get-payment-methods/assert-metadata.ts
+++ src/app/sagas/initialize-app/initialize-available-payment-methods/get-payment-methods/assert-metadata.ts
@@ -1,12 +1,13 @@
 import { ServiceProviderMetadata } from 'checkout/backend';
 import { isNil } from 'lodash-es';
 import { assertServiceFormField } from './assert-service-form-field';
+import { logPrefix } from 'checkout/log-messages';
 
 export function assertMetadata(name: string, metadata: ServiceProviderMetadata) {
     if (!isNil(metadata?.form)) {
         const errors = metadata.form.map((field) => assertServiceFormField(field)).filter((err) => !!err);
         if (errors.length) {
-            console.error(`${name} form has errors: ${errors.join('; ')}`);
+            console.error(`${logPrefix} ${name} form has errors: ${errors.join('; ')}`);
         }
     }
 }
