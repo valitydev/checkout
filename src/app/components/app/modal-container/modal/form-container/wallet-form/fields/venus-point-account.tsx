3609f4d156c230552bcaac107b68f5c53ec0ccc7
Ildar Galeev <KeinAsylum@gmail.com>
2022-03-23 17:28:58 +0300

    Remove email validator

--- src/app/components/app/modal-container/modal/form-container/wallet-form/fields/venus-point-account.tsx
+++ src/app/components/app/modal-container/modal/form-container/wallet-form/fields/venus-point-account.tsx
@@ -3,19 +3,18 @@ import { connect } from 'react-redux';
 import { Field } from 'redux-form';
 
 import { KnownDigitalWalletProviders, State } from 'checkout/state';
-import { formatEmail, validateEmail } from 'checkout/utils';
 import { CustomInputProps, getCustomInput } from './get-custom-input';
 
+const validateAccount = (value: string): boolean => !value || !value.trim();
+
 const VenusPointAccountDef: React.FC<CustomInputProps> = (props) => (
-    <Field name="venusPointAccount" component={getCustomInput.bind(null, props)} validate={validateEmail} />
+    <Field name="venusPointAccount" component={getCustomInput.bind(null, props)} validate={validateAccount} />
 );
 
 const mapStateToProps = (state: State) => ({
     id: 'venus-point-account-input',
     placeholder: state.config.locale['digital.wallet.providers'][KnownDigitalWalletProviders.Venuspoint].fields.account,
-    type: 'email',
-    onInput: formatEmail,
-    autocomplete: 'email'
+    type: 'text'
 });
 
 export const VenusPointAccount = connect(mapStateToProps)(VenusPointAccountDef);
