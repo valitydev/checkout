8bfb8b37a7693d2e64b9e31950e95f35162c121d
Ildar Galeev <KeinAsylum@gmail.com>
2022-04-01 16:58:49 +0300

    Add description for redirect form

--- src/app/components/app/modal-container/modal/form-container/redirect-form/redirect-form.tsx
+++ src/app/components/app/modal-container/modal/form-container/redirect-form/redirect-form.tsx
@@ -1,14 +1,41 @@
 import * as React from 'react';
 import { useEffect, useRef, useState } from 'react';
 
+import styled from 'checkout/styled-components';
 import { useAppDispatch, useAppSelector } from 'checkout/configure-store';
 import { getActiveModalFormSelector } from 'checkout/selectors';
-import { RedirectFormInfo } from 'checkout/state';
+import { RedirectFormInfo, ResultState } from 'checkout/state';
 import { Button } from 'checkout/components';
 import { prepareForm } from 'checkout/utils';
-import { finishInteraction } from 'checkout/actions';
+import { finishInteraction, setResult } from 'checkout/actions';
+
+const RedirectFormContainer = styled.div`
+    visibility: hidden;
+    height: 0;
+`;
+
+const DescriptionContainer = styled.div`
+    height: 160px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const CancelLink = styled.div`
+    text-decoration: underline;
+    cursor: pointer;
+`;
+
+const Container = styled.div`
+    font-weight: 500;
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+`;
 
 export const RedirectForm: React.FC = () => {
+    const locale = useAppSelector((s) => s.config.locale);
     const containerRef = useRef(null);
     const [form, setForm] = useState(null);
     const origin = useAppSelector((s) => s.config.origin);
@@ -26,12 +53,18 @@ export const RedirectForm: React.FC = () => {
         dispatch(finishInteraction());
     };
 
+    const cancel = () => {
+        dispatch(setResult(ResultState.close));
+    };
+
     return (
-        <>
-            <div ref={containerRef}></div>
+        <Container>
+            <RedirectFormContainer ref={containerRef} />
+            <DescriptionContainer>{locale['form.pay.redirect.description']}</DescriptionContainer>
             <Button type="submit" color="primary" onClick={redirect}>
-                Redirect
+                {locale['form.pay.redirect.description.btn.continue']}
             </Button>
-        </>
+            <CancelLink onClick={cancel}>{locale['form.pay.redirect.description.btn.cancel']}</CancelLink>
+        </Container>
     );
 };
