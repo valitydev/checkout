ab21b22a8bea30279955cc8fafed962c88421081
Ildar Galeev <KeinAsylum@gmail.com>
2022-06-07 16:33:42 +0300

    APM-147: QR code form fixes (#98)

--- src/app/components/app/modal-container/modal/form-container/qr-code-interaction-form/qr-code-interaction-form.tsx
+++ src/app/components/app/modal-container/modal/form-container/qr-code-interaction-form/qr-code-interaction-form.tsx
@@ -1,15 +1,32 @@
 import * as React from 'react';
+import { useEffect } from 'react';
+import styled from 'checkout/styled-components';
 
-import { useAppSelector } from 'checkout/configure-store';
-import { getActiveModalFormSelector } from 'checkout/selectors';
+import { useAppDispatch, useAppSelector } from 'checkout/configure-store';
+import { getActiveModalFormSelector, getLocaleSelector } from 'checkout/selectors';
 import { QRCode } from './qr-code';
 import { QrCodeInteractionFormInfo } from 'checkout/state';
+import { finishInteraction } from 'checkout/actions';
+
+const Instruction = styled.p`
+    font-weight: 500;
+    line-height: 24px;
+    text-align: center;
+`;
 
 export const QrCodeInteractionForm: React.FC = () => {
+    const locale = useAppSelector(getLocaleSelector);
     const { request } = useAppSelector<QrCodeInteractionFormInfo>(getActiveModalFormSelector);
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(finishInteraction());
+    }, []);
+
     return (
-        <div>
+        <>
+            <Instruction>{locale['form.qr.code']}</Instruction>
             <QRCode text={request.qrCode} />
-        </div>
+        </>
     );
 };
--- src/app/sagas/poll-events/poll-invoice-events.ts
+++ src/app/sagas/poll-events/poll-invoice-events.ts
@@ -1,10 +1,8 @@
 import last from 'lodash-es/last';
 import { call, put, race, select, delay } from 'redux-saga/effects';
-import { InvoiceEvent, getInvoiceEvents, InvoiceChangeType, InteractionType } from 'checkout/backend';
+import { InvoiceEvent, getInvoiceEvents, InvoiceChangeType } from 'checkout/backend';
 import { SetEventsAction, TypeKeys } from 'checkout/actions';
 import { State } from 'checkout/state';
-import { provideFromInvoiceEvent } from '../provide-modal';
-import get from 'lodash-es/get';
 
 const isStop = (event: InvoiceEvent): boolean => {
     if (!event || !event.changes) {
@@ -31,7 +29,6 @@ function* getLastEventID() {
 function* poll(endpoint: string, token: string, invoiceID: string, interval = 1000) {
     let lastEventID = yield call(getLastEventID);
     let lastEvent = null;
-    let events = [];
     while (!isStop(lastEvent)) {
         yield delay(interval);
         let chunk: InvoiceEvent[] = [];
@@ -46,37 +43,19 @@ function* poll(endpoint: string, token: string, invoiceID: string, interval = 10
         } as SetEventsAction);
         lastEvent = last(chunk);
         lastEventID = lastEvent ? lastEvent.id : lastEventID;
-        events = yield select((state: State) => state.events);
     }
-    return [lastEvent, events];
-}
-
-function isEventToWait(event: InvoiceEvent): boolean {
-    return (
-        event &&
-        get(last(event.changes), ['userInteraction', 'interactionType']) === InteractionType.QrCodeDisplayRequest
-    );
+    return lastEvent;
 }
 
 const POLLING_TIME_MS = 60 * 1000 * 5;
 const POLLING_INTEVAL_MS = 1000;
-const EVENTS_WAIT_POLLING_TIME_MS = 10 * 60 * 1000;
-const EVENTS_WAIT_INTERVAL_MS = 5 * 1000;
 
 export function* pollInvoiceEvents(endpoint: string, token: string, invoiceID: string) {
     let lastEvent: InvoiceEvent;
-    let events: InvoiceEvent[];
     for (let i = 1; !lastEvent && i < 6; i += 1) {
-        [[lastEvent, events]] = yield race<any>([
+        [lastEvent] = yield race<any>([
             call(poll, endpoint, token, invoiceID),
             delay(POLLING_TIME_MS * i, POLLING_INTEVAL_MS * 2 ** i)
-        ]);
-    }
-    if (isEventToWait(lastEvent)) {
-        yield call(provideFromInvoiceEvent, events);
-        [lastEvent] = yield race([
-            call(poll, endpoint, token, invoiceID, EVENTS_WAIT_INTERVAL_MS),
-            delay(EVENTS_WAIT_POLLING_TIME_MS)
         ]);
     }
     if (lastEvent) {
