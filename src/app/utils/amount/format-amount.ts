2fb8649cf55620a990f6fbdaa62f25dc0f8bcbf2
Ildar Galeev <KeinAsylum@gmail.com>
2022-06-29 19:04:00 +0300

    Add currency exponent calculation

--- src/app/utils/amount/format-amount.ts
+++ src/app/utils/amount/format-amount.ts
@@ -1,8 +1,14 @@
+import * as Currencies from '@dinero.js/currencies';
+
 import { AmountInfoState } from 'checkout/state';
 
-export const formatAmount = (amount: AmountInfoState): string | null =>
-    amount && amount.minorValue
+const getAmountByExponent = ({ minorValue, currencyCode }: AmountInfoState): number =>
+    minorValue / Number('1e' + Currencies[currencyCode].exponent);
+
+export const formatAmount = (amount: AmountInfoState): string | null => {
+    return amount && amount.minorValue
         ? new Intl.NumberFormat(amount.locale, { style: 'currency', currency: amount.currencyCode }).format(
-              amount.minorValue / 100
+              getAmountByExponent(amount)
           )
         : null;
+};
