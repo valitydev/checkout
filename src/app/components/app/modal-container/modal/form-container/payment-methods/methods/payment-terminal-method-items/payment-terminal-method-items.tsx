96712487246fad45b71deb080aec0ea0465135d0
Ildar Galeev <KeinAsylum@gmail.com>
2022-07-06 15:37:47 +0300

    Removed useless method items

--- src/app/components/app/modal-container/modal/form-container/payment-methods/methods/payment-terminal-method-items/payment-terminal-method-items.tsx
+++ src/app/components/app/modal-container/modal/form-container/payment-methods/methods/payment-terminal-method-items/payment-terminal-method-items.tsx
@@ -3,11 +3,8 @@ import * as React from 'react';
 import { KnownProviderCategories, PaymentTerminalPaymentMethod } from 'checkout/state';
 import { assertUnreachable } from 'checkout/utils';
 import { Locale } from 'checkout/locale';
-import { OnlineBankingMethodItem } from './online-banking-method-item';
-import { UPIPaymentMethodItem } from './upi-payment-method-item';
 import { PaymentTerminalBankCardMethodItem } from './payment-terminal-bank-card-method-item';
 import { PayAction, SetFormInfoAction } from './types';
-import { PaymentTerminalDigitalWalletsMethodItem } from './payment-terminal-digital-wallets-method-item';
 import { PaymentTerminalMethodItem } from './payment-terminal-method-item';
 
 export interface PaymentTerminalMethodItemsProps {
@@ -26,14 +23,12 @@ export const PaymentTerminalMethodItems: React.FC<PaymentTerminalMethodItemsProp
     localeCode
 }) => {
     switch (method.category) {
-        case KnownProviderCategories.OnlineBanking:
-        case KnownProviderCategories.NetBanking:
-            return <OnlineBankingMethodItem method={method} locale={locale} setFormInfo={setFormInfo} pay={pay} />;
         case KnownProviderCategories.UPI:
-            return <UPIPaymentMethodItem method={method} setFormInfo={setFormInfo} pay={pay} />;
         case KnownProviderCategories.PIX:
         case KnownProviderCategories.PaymentTerminal:
         case KnownProviderCategories.DigitalWallet:
+        case KnownProviderCategories.OnlineBanking:
+        case KnownProviderCategories.NetBanking:
             return (
                 <PaymentTerminalMethodItem
                     method={method}
@@ -44,8 +39,6 @@ export const PaymentTerminalMethodItems: React.FC<PaymentTerminalMethodItemsProp
             );
         case KnownProviderCategories.BankCard:
             return <PaymentTerminalBankCardMethodItem locale={locale} setFormInfo={setFormInfo} />;
-        // case KnownProviderCategories.DigitalWallet:
-        //     return <PaymentTerminalDigitalWalletsMethodItem method={method} locale={locale} pay={pay} />;
         default:
             assertUnreachable(method.category);
             return null;
