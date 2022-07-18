b16c95a1b760fa5f283e06d02a6ef8e1e84ea04a
Ildar Galeev <KeinAsylum@gmail.com>
2022-07-18 21:00:25 +0300

    Add contact info to terminal bank card form

--- src/app/components/app/modal-container/modal/form-container/payment-terminal-bank-card-form/payment-terminal-bank-card-form.tsx
+++ src/app/components/app/modal-container/modal/form-container/payment-terminal-bank-card-form/payment-terminal-bank-card-form.tsx
@@ -1,16 +1,20 @@
 import * as React from 'react';
 import { useEffect } from 'react';
+import { InjectedFormProps, reduxForm } from 'redux-form';
 
 import { FormName, KnownProviderCategories, PaymentTerminalFormValues } from 'checkout/state';
 import { Header } from '../header';
-import { getAvailableTerminalPaymentMethodSelector } from 'checkout/selectors';
+import { getAvailableTerminalPaymentMethodSelector, getInitConfigSelector } from 'checkout/selectors';
 import { useAppDispatch, useAppSelector } from 'checkout/configure-store';
 import { ProviderSelectorField } from './provider-selector';
-import { InjectedFormProps, reduxForm } from 'redux-form';
 import { pay, setViewInfoError } from 'checkout/actions';
 import { PayButton } from '../pay-button';
 import { PaymentMethodName } from 'checkout/backend';
 import styled from 'checkout/styled-components';
+import { toEmailConfig, toPhoneNumberConfig } from '../fields-config';
+import { FormGroup } from '../form-group';
+import { Email, Phone } from '../common-fields';
+import { getMetadata } from 'checkout/components';
 
 const ProviderSelectorDescription = styled.p`
     font-size: 16px;
@@ -19,10 +23,14 @@ const ProviderSelectorDescription = styled.p`
     margin: 0 0 8px 0;
 `;
 
-export const PaymentTerminalBankCardFormDef: React.FC<InjectedFormProps> = (props) => {
+export const PaymentTerminalBankCardFormDef: React.FC<InjectedFormProps> = ({ submitFailed, handleSubmit }) => {
+    const initConfig = useAppSelector(getInitConfigSelector);
     const locale = useAppSelector((s) => s.config.locale);
     const paymentMethod = useAppSelector(getAvailableTerminalPaymentMethodSelector(KnownProviderCategories.BankCard));
     const serviceProviders = paymentMethod?.serviceProviders;
+    const email = toEmailConfig(initConfig.email);
+    const phoneNumber = toPhoneNumberConfig(initConfig.phoneNumber);
+    const { contactInfo } = getMetadata(serviceProviders && serviceProviders[0]);
     const dispatch = useAppDispatch();
 
     useEffect(() => {
@@ -30,25 +38,33 @@ export const PaymentTerminalBankCardFormDef: React.FC<InjectedFormProps> = (prop
     }, []);
 
     useEffect(() => {
-        if (props.submitFailed) {
+        if (submitFailed) {
             dispatch(setViewInfoError(true));
         }
-    }, [props.submitFailed]);
+    }, [submitFailed]);
 
-    const submit = ({ provider }: PaymentTerminalFormValues) => {
+    const submit = (values: PaymentTerminalFormValues) => {
         dispatch(
             pay({
                 method: PaymentMethodName.PaymentTerminal,
-                values: {
-                    provider
-                } as PaymentTerminalFormValues
+                values
             })
         );
     };
 
     return (
-        <form onSubmit={props.handleSubmit(submit)}>
+        <form onSubmit={handleSubmit(submit)}>
             <Header title={locale['form.header.pay.card.label']} />
+            {email.visible && contactInfo?.email && (
+                <FormGroup>
+                    <Email />
+                </FormGroup>
+            )}
+            {phoneNumber.visible && contactInfo?.phoneNumber && (
+                <FormGroup>
+                    <Phone />
+                </FormGroup>
+            )}
             <ProviderSelectorDescription>
                 {locale['form.pay.paymentTerminalBankCard.providerSelectorDescription']}
             </ProviderSelectorDescription>
