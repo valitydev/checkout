ce4e38bf9edeae354ed815c8bf7a6618d6704c3e
Ildar Galeev <KeinAsylum@gmail.com>
2023-01-11 19:43:06 +0700

    Bump sentry

--- src/app/sagas/initialize-app/initialize-app.ts
+++ src/app/sagas/initialize-app/initialize-app.ts
@@ -23,7 +23,7 @@ export function* initialize(userInitConfig: InitConfig) {
             environment: 'production',
             dsn: configChunk.appConfig.sentryDsn,
             integrations: [new Integrations.BrowserTracing()],
-            tracesSampleRate: 1.0,
+            tracesSampleRate: 0.2,
             release: configChunk.env.version
         });
     }
