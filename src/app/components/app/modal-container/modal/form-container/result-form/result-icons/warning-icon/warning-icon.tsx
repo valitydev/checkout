50c9541cdca5dabcbe5bb00a104df3d1159f30bc
Rinat Arsaev <11846445+A77AY@users.noreply.github.com>
2023-02-07 17:30:16 +0600

    add ids

--- src/app/components/app/modal-container/modal/form-container/result-form/result-icons/warning-icon/warning-icon.tsx
+++ src/app/components/app/modal-container/modal/form-container/result-form/result-icons/warning-icon/warning-icon.tsx
@@ -3,7 +3,7 @@ import { withTheme } from 'checkout/styled-components';
 import { WithThemeProps } from 'checkout/themes';
 
 const WarningIconDef: React.FC<WithThemeProps> = ({ theme }) => (
-    <svg width="144" height="144" viewBox="0 0 144 144" fill="none">
+    <svg width="144" height="144" viewBox="0 0 144 144" fill="none" id="warning-icon">
         <circle cx="72" cy="72" r="70" stroke={theme.icons.warn} strokeWidth="4" />
         <rect x="67" y="32" width="10" height="64" rx="2" fill={theme.icons.warn} />
         <rect x="67" y="102" width="10" height="10" rx="2" fill={theme.icons.warn} />
