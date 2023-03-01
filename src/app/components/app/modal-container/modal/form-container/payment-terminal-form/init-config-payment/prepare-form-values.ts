d1d0f9b4e5b9f8272cee9949c117de474c5c2671
Ildar Galeev <KeinAsylum@gmail.com>
2023-03-01 17:32:07 +0700

    Add replaceValuePattern metadata param (#170)

--- src/app/components/app/modal-container/modal/form-container/payment-terminal-form/init-config-payment/prepare-form-values.ts
+++ src/app/components/app/modal-container/modal/form-container/payment-terminal-form/init-config-payment/prepare-form-values.ts
@@ -1,7 +1,16 @@
 import isNil from 'lodash-es/isNil';
+import isString from 'lodash-es/isString';
 
 import { ServiceProviderMetadataField } from 'checkout/backend';
 import { PaymentTerminalFormValues } from 'checkout/state';
+
+const applyReplacePattern = <T>(rawValue: T, pattern?: string, replaceValue = ''): string | T => {
+    if (!isNil(pattern) && isString(rawValue)) {
+        const regExp = new RegExp(pattern, 'g');
+        return rawValue.replaceAll(regExp, replaceValue);
+    }
+    return rawValue;
+};
 
 export const prepareFormValues = (
     form: ServiceProviderMetadataField[],
@@ -12,12 +21,13 @@ export const prepareFormValues = (
         if (isNil(value)) {
             return acc;
         }
-        if (formField.pattern && !new RegExp(formField.pattern).test(value)) {
+        const appliedValue = applyReplacePattern(value, formField.replaceValuePattern);
+        if (formField.pattern && !new RegExp(formField.pattern).test(appliedValue)) {
             return acc;
         }
         return {
             ...acc,
-            [formField.name]: value
+            [formField.name]: appliedValue
         };
     }, {})
 });
