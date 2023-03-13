d23f22d32619e4f78a43122ca1f47e1c2bb26eda
Ildar Galeev <KeinAsylum@gmail.com>
2023-03-13 20:03:38 +0700

    Add korean locale. Adjust sentry trace rate. (#171)

--- src/locale/detect-locale.ts
+++ src/locale/detect-locale.ts
@@ -1,6 +1,6 @@
 import { isIE } from './is-ie';
 
-const locales = ['ru', 'en', 'ja', 'pt', 'bn'];
+const locales = ['ru', 'en', 'ja', 'pt', 'bn', 'ko'];
 
 export const detectLocale = (locale: string = 'auto'): string => {
     let result;
