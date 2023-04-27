eb29391466582e54b95796867e093010a5cf690f
Ildar Galeev <KeinAsylum@gmail.com>
2023-04-27 21:43:35 +0700

    Remove initialize app saga

--- src/app/sagas/amount-info/resolve-invoice.ts
+++
@@ -1,9 +0,0 @@
-import { Invoice } from 'checkout/backend';
-import { AmountInfoState, AmountInfoStatus } from 'checkout/state';
-
-export const resolveInvoice = ({ amount, currency }: Invoice, locale: string): AmountInfoState => ({
-    status: AmountInfoStatus.final,
-    minorValue: amount,
-    currencyCode: currency,
-    locale
-});
