150b94f345beadda4f3be86d6e3e353ce7858e28
Ildar Galeev <KeinAsylum@gmail.com>
2023-06-05 16:18:33 +0300

    Remove unused code

--- src/app/actions/index.ts
+++ src/app/actions/index.ts
@@ -1,4 +1,3 @@
-export * from './result-action';
 export * from './type-keys';
 export * from './abstract-action';
 export * from './modal-actions';
--- src/app/state/state.ts
+++ src/app/state/state.ts
@@ -1,7 +1,6 @@
-import { ResultState, FormsState, ModalState } from '.';
+import { FormsState, ModalState } from '.';
 
 export interface State {
-    readonly result: ResultState;
     readonly form: FormsState;
     readonly modals: ModalState[];
 }
