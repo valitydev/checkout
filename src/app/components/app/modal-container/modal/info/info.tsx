51590dde0a3b5799772c40bffea004510f016beb
Ildar Galeev <KeinAsylum@gmail.com>
2023-08-23 16:35:03 +0300

    Add history back button for mobile devices

--- src/app/components/app/modal-container/modal/info/info.tsx
+++ src/app/components/app/modal-container/modal/info/info.tsx
@@ -1,60 +1,54 @@
+import isMobile from 'ismobilejs';
 import { useContext, useMemo } from 'react';
 import styled from 'styled-components';
 
 import { formatAmount } from 'checkout/utils';
 import { device } from 'checkout/utils/device';
 
+import { Back } from './back';
 import { InitialContext } from '../../../initial-context';
 
 const InfoWrapper = styled.div`
     padding: 16px;
     display: flex;
     flex-direction: column;
-    gap: 8px;
+    gap: 24px;
 
     @media ${device.desktop} {
         padding: 24px 0;
         width: 230px;
         margin-right: 30px;
-        gap: 24px;
     }
 `;
 
-const CompanyName = styled.h4`
+const CompanyName = styled.p`
     font-weight: 500;
-    font-size: 16px;
+    font-size: 18px;
     color: ${({ theme }) => theme.font.primaryColor};
-    letter-spacing: 0;
-    line-height: 20px;
     margin: 0;
 `;
 
-const Amount = styled.h1`
+const Amount = styled.p`
     font-weight: 500;
-    font-size: 30px;
+    font-size: 32px;
     color: ${({ theme }) => theme.font.primaryColor};
-    letter-spacing: 0;
-    line-height: 35px;
     margin: 0;
 `;
 
-const Order = styled.div`
+const Order = styled.p`
     font-weight: 900;
-    font-size: 11px;
+    font-size: 12px;
     color: ${({ theme }) => theme.font.primaryColor};
     opacity: 0.5;
     letter-spacing: 2px;
-    line-height: 15px;
     text-transform: uppercase;
     margin: 0;
 `;
 
-const ProductDescription = styled.div`
+const ProductDescription = styled.p`
     font-weight: 500;
     font-size: 16px;
     color: ${({ theme }) => theme.font.primaryColor};
-    letter-spacing: 0;
-    line-height: 20px;
     margin: 0;
 `;
 
@@ -66,11 +60,16 @@ const DescriptionContainer = styled.div`
 
 export const Info = () => {
     const { locale, initConfig, amountInfo } = useContext(InitialContext);
-
     const formattedAmount = useMemo(() => formatAmount(amountInfo), [amountInfo]);
+    const isBackVisible = useMemo(() => isMobile(window.navigator).phone || isMobile(window.navigator).tablet, []);
 
     return (
         <InfoWrapper>
+            {isBackVisible && (
+                <div>
+                    <Back locale={locale} />
+                </div>
+            )}
             {initConfig.name ? <CompanyName id="company-name-label">{initConfig.name}</CompanyName> : false}
             {formattedAmount ? <Amount>{formattedAmount}</Amount> : false}
             {initConfig.description && (
