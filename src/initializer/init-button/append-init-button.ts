93eb5e47ade26f790510003330a31a3b18246647
Ildar Galeev <KeinAsylum@gmail.com>
2024-02-19 20:18:27 +0800

    Fix build

--- src/initializer/init-button/append-init-button.ts
+++ src/initializer/init-button/append-init-button.ts
@@ -1,7 +1,7 @@
 import { initButton } from './init-button.css';
 import { detectLocale } from '../../locale';
 
-const getDefaultLabel = (): string => (detectLocale() === 'ru' ? 'Оплатить' : 'Pay');
+const getDefaultLabel = (): string => (detectLocale(null) === 'ru' ? 'Оплатить' : 'Pay');
 
 const appendPayButtonStyles = (origin: string) => {
     const link = document.createElement('link');
