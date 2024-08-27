d1cd7d1f5714d766e5eb9949a957c30a90ab7f5c
Ildar Galeev <KeinAsylum@gmail.com>
2024-08-27 17:04:45 +0300

    IMP-305: Add TJ locale

--- src/components/ViewContainer/ApiExtensionView/utils/getGatewayIcon.tsx
+++ src/components/ViewContainer/ApiExtensionView/utils/getGatewayIcon.tsx
@@ -1,9 +1,13 @@
 import { SberbankIcon, TinkoffIcon, RaiffeisenIcon, SBPIcon } from '../icons';
 
 export const getGatewayIcon = (gatewayName: string, defaultIcon?: JSX.Element): JSX.Element => {
-    switch (gatewayName) {
+    switch (gatewayName.toLowerCase()) {
         case 'sberbank':
-        case 'Сбербанк':
+        case 'сбер':
+        case 'sber':
+        case 'сбербанк':
+        case 'сбр':
+        case 'sbr':
             return <SberbankIcon />;
         case 'tinkoff':
             return <TinkoffIcon />;
--- src/components/ViewContainer/ApiExtensionView/utils/mapGatewayName.ts
+++ src/components/ViewContainer/ApiExtensionView/utils/mapGatewayName.ts
@@ -2,5 +2,5 @@ import { Locale } from 'checkout/contexts';
 
 export const mapGatewayName = (gatewayName: string, l: Locale): string => {
     const gateways = l?.['p2p.gateways'] ?? {};
-    return gateways[gatewayName] ?? gatewayName;
+    return gateways[gatewayName.toLowerCase()] ?? gatewayName;
 };
