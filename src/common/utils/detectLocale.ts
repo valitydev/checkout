b61afed1b19831c0b8384a50b797baa5fa8aadc9
Ildar Galeev <KeinAsylum@gmail.com>
2025-03-25 14:47:37 +0300

    Upd p2p notifications (#397)

--- src/common/utils/detectLocale.ts
+++ src/common/utils/detectLocale.ts
@@ -1,6 +1,6 @@
 import { isNil } from './isNil';
 
-const locales = ['ru', 'en', 'az', 'pt', 'tj', 'tr', 'uz', 'kk'];
+const locales = ['ru', 'en', 'az', 'pt', 'tj', 'uz', 'kk'];
 
 export const detectLocale = (locale: string | null): string => {
     let result;
